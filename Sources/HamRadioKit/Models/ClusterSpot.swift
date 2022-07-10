f942ea4085ab484d85b2044b42cc72e8ef8ec516
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2022-07-10 13:36:49 +0200

    Update ClusterSpot model

--- Sources/HamRadioKit/Models/ClusterSpot.swift
+++ Sources/HamRadioKit/Models/ClusterSpot.swift
@@ -7,12 +7,22 @@
 
 import Foundation
 
-struct ClusterSpot: Codable {
-    let callsign: String
-    let spotter: String
-    let dxccId: DXCC?
-    let frequency: Frequency
-    let time: Date
-    let comment: String
-    let gridsquare: Locator?
+public struct ClusterSpot: Codable {
+    public let id: UUID
+    public let callsign: String
+    public let spotter: String
+    public let frequency: Frequency
+    public let timestamp: Date
+    public let comment: String?
+    public let gridsquare: Locator?
+
+    public init(id: UUID, callsign: String, spotter: String, frequency: Frequency, timestamp: Date, comment: String?, gridsquare: Locator?) {
+        self.id = id
+        self.callsign = callsign
+        self.spotter = spotter
+        self.frequency = frequency
+        self.timestamp = timestamp
+        self.comment = comment
+        self.gridsquare = gridsquare
+    }
 }
